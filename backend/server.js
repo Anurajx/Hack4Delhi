3d93d9df4e2aaa7fab2035c736cfcc28c61a1baa
Anurajx <anurajupadhyay6@gmail.com>
2026-01-11 01:04:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 /* -----------------------------------
    DATABASE CONNECTIONS
------------------------------------ */
+------------------------------------ */
 
 const DB = mongoose.createConnection(process.env.MONGO_URI, {
   dbName: "StateVoter",
